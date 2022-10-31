032ba0a0c6ec18ff11d56bafe39559ba205bf891
Yujie Wang <yujie@wang.icu>
2022-10-31 00:38:14 +0000

    format GPIO rtl, add README

--- rtl/AHB_GPIO/AHBGPIO.v
+++ rtl/AHB_GPIO/AHBGPIO.v
@@ -35,90 +35,76 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module AHBGPIO(
-  input wire HCLK,
-  input wire HRESETn,
-  input wire [31:0] HADDR,
-  input wire [1:0] HTRANS,
-  input wire [31:0] HWDATA,
-  input wire HWRITE,
-  input wire HSEL,
-  input wire HREADY,
-  input wire [15:0] GPIOIN,
-  
-	
-	//Output
-  output wire HREADYOUT,
-  output wire [31:0] HRDATA,
-  output wire [15:0] GPIOOUT
-  
-  
-  );
-  
-  localparam [7:0] gpio_data_addr = 8'h00;
-  localparam [7:0] gpio_dir_addr = 8'h04;
-  
+module AHBGPIO (
+    input wire HCLK,
+    input wire HRESETn,
+    input wire [31:0] HADDR,
+    input wire [1:0] HTRANS,
+    input wire [31:0] HWDATA,
+    input wire HWRITE,
+    input wire HSEL,
+    input wire HREADY,
+    input wire [15:0] GPIOIN,
+
+    //Output
+    output wire HREADYOUT,
+    output wire [31:0] HRDATA,
+    output wire [15:0] GPIOOUT
+);
+
+  localparam byte [7:0] gpio_data_addr = 8'h00;
+  localparam byte [7:0] gpio_dir_addr = 8'h04;
+
   reg [15:0] gpio_dataout;
   reg [15:0] gpio_datain;
   reg [15:0] gpio_dir;
   reg [15:0] gpio_data_next;
   reg [31:0] last_HADDR;
   reg [1:0] last_HTRANS;
+
   reg last_HWRITE;
   reg last_HSEL;
-  
+
   integer i;
-  
+
   assign HREADYOUT = 1'b1;
-  
-// Set Registers from address phase  
-  always @(posedge HCLK)
-  begin
-    if(HREADY)
-    begin
-      last_HADDR <= HADDR;
+
+  // Set Registers from address phase
+  always @(posedge HCLK) begin
+    if (HREADY) begin
+      last_HADDR  <= HADDR;
       last_HTRANS <= HTRANS;
       last_HWRITE <= HWRITE;
-      last_HSEL <= HSEL;
+      last_HSEL   <= HSEL;
     end
   end
 
   // Update in/out switch
-  always @(posedge HCLK, negedge HRESETn)
-  begin
-    if(!HRESETn)
-    begin
+  always @(posedge HCLK, negedge HRESETn) begin
+    if (!HRESETn) begin
       gpio_dir <= 16'h0000;
-    end
-    else if ((last_HADDR[7:0] == gpio_dir_addr) & last_HSEL & last_HWRITE & last_HTRANS[1])
+    end else if ((last_HADDR[7:0] == gpio_dir_addr) & last_HSEL & last_HWRITE & last_HTRANS[1])
       gpio_dir <= HWDATA[15:0];
   end
-  
+
   // Update output value
-  always @(posedge HCLK, negedge HRESETn)
-  begin
-    if(!HRESETn)
-    begin
+  always @(posedge HCLK, negedge HRESETn) begin
+    if (!HRESETn) begin
       gpio_dataout <= 16'h0000;
     end
     else if ((gpio_dir == 16'h0001) & (last_HADDR[7:0] == gpio_data_addr) & last_HSEL & last_HWRITE & last_HTRANS[1])
       gpio_dataout <= HWDATA[15:0];
   end
-  
+
   // Update input value
-  always @(posedge HCLK, negedge HRESETn)
-  begin
-    if(!HRESETn)
-    begin
+  always @(posedge HCLK, negedge HRESETn) begin
+    if (!HRESETn) begin
       gpio_datain <= 16'h0000;
-    end
-    else if (gpio_dir == 16'h0000)
-      gpio_datain <= GPIOIN;
-    else if (gpio_dir == 16'h0001)
-      gpio_datain <= GPIOOUT;
+    end else if (gpio_dir == 16'h0000) gpio_datain <= GPIOIN;
+    else if (gpio_dir == 16'h0001) gpio_datain <= GPIOOUT;
   end
-         
-  assign HRDATA[15:0] = gpio_datain;  
+
+  assign HRDATA[15:0] = gpio_datain;
   assign GPIOOUT = gpio_dataout;
 
 endmodule
