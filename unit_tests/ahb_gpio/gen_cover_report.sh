e1beb64d925430c2b8e5c2f283a50c8e33ee14a6
Yujie Wang <yujie@wang.icu>
2022-12-11 18:40:02 +0000

    move loopback to sub dir

--- unit_tests/ahb_gpio/gen_cover_report.sh
+++ unit_tests/ahb_gpio/gen_cover_report.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-UCDB_FILE=testdb.ucdb
+UCDB_FILE=loopback.ucdb
 
 # vsim -c -cvgperinstance -viewcov "${UCDB_FILE}" -do "coverage report -file final_report.txt -byfile -detail -noannotate -option -cvg"
 vcover report -details -html "${UCDB_FILE}"
