a1084da98e20b4597c7c0c9ace87da3a0a932ea2
Yujie Wang <yujie@wang.icu>
2022-12-16 20:26:59 +0000

    Done\!

--- system_tests/ahblite_sys_tb.sv
+++ system_tests/ahblite_sys_tb.sv
@@ -2,28 +2,42 @@
 
 module ahblite_sys_tb #(
   parameter int WIDTH  = 705,
-  parameter int HEIGHT = 50
+  parameter int HEIGHT = 520
 ) ();
 
   bit RESET, CLK;
 
-  wire [7:0] LED;
+  wire  [16:0] LED;
+  logic [16:0] SW;
+  wire         PARITYSEL;
+  wire         PARITYERR;
+
   wire HSYNC;
   wire VSYNC;
   wire [7:0] RGB;
 
   always #5 CLK = ~CLK;
+  assign PARITYSEL=$urandom()%2;
 
   initial begin
     RESET=0;
     #50 RESET=1;
   end
 
+  always @(posedge CLK or negedge RESET) begin
+    if (!RESET) begin
+      SW <= 17'h0ff;
+    end else begin
+      SW <= SW + 1;
+    end
+  end
+
+
   AHBLITE_SYS dut(
     // Clocking
     .CLK(CLK), .RESET(RESET), 
     // GPIO
-    .LED(LED),
+    .LED(LED), .SW(SW), .PARITYSEL(PARITYSEL), .PARITYERR(PARITYERR),
     // VGA
     .HSYNC(HSYNC), .VSYNC(VSYNC),
     .VGARED(RGB[7:5]), .VGAGREEN(RGB[4:2]), .VGABLUE(RGB[1:0])
@@ -51,11 +65,17 @@ module ahblite_sys_tb #(
         cnt++;
     end
   endtask
-
+ 
   initial begin
     $display("start test");
     monitor_for_frame();
     $finish;
+  end
+
+  //enabling the wave dump
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
   end
 
 endmodule
